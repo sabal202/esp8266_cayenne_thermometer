3796dd2d7b064686c7cc74f59a7e8fa4bba9ef0f
Sabalevskiy Sergey <sabal2000@mail.ru>
2017-12-03 17:57:12 +0700

    removed OTA

--- ESP8266_cayenne.ino
+++ ESP8266_cayenne.ino
@@ -1,70 +1,28 @@
 #include <CayenneMQTTESP8266.h>
 #include <DHT.h>
-#include <ESP8266WiFi.h>
-#include <ESP8266mDNS.h>
-#include <WiFiUdp.h>
-#include <ArduinoOTA.h>
 
 #define DHT_PIN 2
 
 DHT dht(DHT_PIN, DHT22);
 
-const char* ssid = "ddd";
-const char* wifiPassword = "ddd";
+char ssid[] = "";
+char wifiPassword[] = "";
 
-char username[] = "ddd";
-char password[] = "ddd";
-char clientID[] = "ddd";
+char username[] = "e4ca7f00-645011";
+char password[] = "4275a8c8bd00181860";
+char clientID[] = "2d07faebe02555";
 
 unsigned long lastMillis = 0;
 
 void setup() {
-  Serial.begin(115200);
-  Serial.println("Booting...");
-  
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, wifiPassword);
-  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
-    Serial.println("Connection Failed!\nRebooting...");
-    delay(5000);
-    ESP.restart();
-  }
-
-  //ArduinoOTA.setPort(8266);
-  //ArduinoOTA.setHostname("myesp8266_thermometer");
-  //ArduinoOTA.setPassword((const char *)"sabal");
-
-  ArduinoOTA.onStart([]() {
-    Serial.println("Start");
-  });
-  ArduinoOTA.onEnd([]() {
-    Serial.println("\nEnd");
-  });
-  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-  });
-  ArduinoOTA.onError([](ota_error_t error) {
-    Serial.printf("Error[%u]: ", error);
-    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-    else if (error == OTA_END_ERROR) Serial.println("End Failed");
-  });
-  ArduinoOTA.begin();
-  Serial.println("Ready");
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-  
+  Serial.begin(9600);
   Cayenne.begin(username, password, clientID, ssid, wifiPassword);
+  Serial.println("Ready");
 }
 
 void loop() {
-  Serial.println("ArduinoOTA.handle();");
-  ArduinoOTA.handle();
-  Serial.println("Cayenne.loop();");
   Cayenne.loop();
-  Serial.println("End");
+
   if (millis() - lastMillis > 3000) {
     lastMillis = millis();
 
@@ -78,5 +36,4 @@ void loop() {
 
     Serial.println("");
   }
-  
 }
